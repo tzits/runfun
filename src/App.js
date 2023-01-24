20d7bf28d89e2af7fc2ceb5f14f075d64f6cecad
Toby <tzitsman@gmail.com>
2023-01-24 11:56:09 -0500

    create nav component and move logic

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
 import React, { Component } from 'react'
 import './App.css'
-import Men from './components/Men'
-import Women from './components/Women'
-import MastersWomen from './components/MastersWomen'
-import Grandmasters from './components/Grandmasters'
-import Entry from './components/Entry'
 import {
   BrowserRouter,
-  Routes,
-  Route,
-  Link,
   Outlet,
-  useParams
 } from 'react-router-dom'
+import Nav from './components/Nav/Nav.component'
 
 class App extends Component {
 
   render() {
+
     return(
       <div className="App">
-      <BrowserRouter>
-        <div>
-          <div className='nav'>
-            <span className='nav_item'><Link to='/'>Home</Link></span>
-            <span className='nav_item'><Link to='/men'>Men</Link></span>
-            <span className='nav_item'><Link to='/women'>Women</Link></span>
-            <span className='nav_item'><Link to='/mwomen'>Masters Women</Link></span>
-            <span className='nav_item'><Link to='/gmen'>Grandmasters</Link></span>
-          </div>
-
-          <hr />
-
-          <Routes>
-            <Route path='/' element={<Entry />} />
-            <Route path='/men' element={<Men />} />
-            <Route path='/women' element={<Women />} />
-            <Route path='/mwomen' element={<MastersWomen />} />
-            <Route path='/gmen' element={<Grandmasters />} />
-          </Routes>
-
-        </div>
-        <Outlet />
-      </BrowserRouter>
-
-
-
+        <BrowserRouter>
+          <Nav />
+          <Outlet />
+        </BrowserRouter>
       </div>
 
     )
   }
 }
-
-
-
-
 
 export default App;
