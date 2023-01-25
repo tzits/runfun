e4042dd07cf803db1dc0a7921f19ef8e54b491b0
Toby <tzitsman@gmail.com>
2023-01-25 17:42:04 -0500

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,186 @@
+export const pointRaces = [
+    {
+        name: 'Coogans',
+        date: '03/05/2023',
+        registrationLink: 'https://www.nyrr.org/races/nyrrwashingtonheightssalsabluesandshamrocks5k',
+        distance: '5k',
+        location: 'Washington Heights' 
+    },
+    {
+        name: 'CP 4miler',
+        date: '04/23/2023',
+        registrationLink: 'https://www.nyrr.org/races/rbcraceforthekids4mpresentedbynyrr',
+        distance: '4M',
+        location: 'Central Park'
+    },    
+    {
+        name: 'Brooklyn Half',
+        date: '05/20/2023',
+        registrationLink: 'https://www.nyrr.org/races/rbcbrooklynhalf',
+        distance: '13.1',
+        location: 'Brooklyn'
+
+    },    
+    {
+        name: 'Queens 10k',
+        date: '06/17/2023',
+        registrationLink: 'https://www.nyrr.org',
+        distance: '10k',
+        location: 'Queens'
+    },    
+    {
+        name: 'Team Champs',
+        date: '07/30/2023',
+        registrationLink: 'https://www.nyrr.org',
+        distance: '5M',
+        location: 'Central Park'
+    },    
+    {
+        name: 'Harlem 5k',
+        date: '08/12/2023',
+        registrationLink: 'https://www.nyrr.org',
+        distance: '5k',
+        location: 'Harlem'
+    },    
+    {
+        name: '5th Ave Mile',
+        date: '09/10/2023',
+        registrationLink: 'https://www.nyrr.org/',
+        distance: '1M',
+        location: '5th Avenue'
+    },    
+    {
+        name: 'Bronx 10 Miler',
+        date: '09/17/2023',
+        registrationLink: 'https://www.nyrr.org/',
+        distance: '10M',
+        location: 'Bronx'
+    },    
+    {
+        name: 'Staten Island Half',
+        date: '10/08/2023',
+        registrationLink: 'https://www.nyrr.org/',
+        distance: '13.1',
+        location: 'Staten Island'
+    },    
+    {
+        name: 'NYC Marathon',
+        date: '11/05/2023',
+        registrationLink: 'https://www.nyrr.org/',
+        distance: '26.2',
+        location: 'New York'
+    },    
+    {
+        name: 'Ted Corbitt',
+        date: '03/05/2023',
+        registrationLink: 'https://www.nyrr.org/',
+        distance: '5k',
+        location: 'Central Park'
+    }
+]
+
+export const otherRaces = [
+    {
+    },
+]
+
+export const startingResults = 
+    {
+        men:
+        [
+          {
+            name: 'Toby Zitsman',
+            score: 32
+          }, 
+          {
+            name: 'Jay Schairbaum',
+            score: 20
+          },
+          {
+            name: 'Ryan Phillips',
+            score: 18
+          },
+          {
+            name: 'Greg Clark',
+            score: 36
+          },
+          {
+            name: 'James Gusmer',
+            score: 6
+          },      {
+            name: 'Nick Adams',
+            score: 34
+          },      {
+            name: 'Conor Lanz',
+            score: 26
+          },      {
+            name: 'Stephen Johnson',
+            score: 26
+          },      {
+            name: 'Kyle Vines',
+            score: 16
+          },      {
+            name: "Ciaran O'Donovan",
+            score: 12
+          },      {
+            name: 'Scott Easey',
+            score: 32
+          }
+        ],
+        women:
+        [
+          {
+            name: 'Kaitlyn DiBello',
+            score: 42
+          }, 
+          {
+            name: 'Becca Ades',
+            score: 64
+          },
+          {
+            name: 'Taeya Konishi Schogel',
+            score: 32
+          },
+          {
+            name: 'Gabby Tofig',
+            score: 20
+          },
+          {
+            name: 'Anna Hope Emerson',
+            score: 12
+          },
+          {
+            name: 'Danielle Aspitz',
+            score: 24
+          },
+          {
+            name: 'Emma Raub',
+            score: 20
+          },
+          {
+            name: 'Margaret Geary',
+            score: 12
+          },
+          {
+            name: 'Alyssa Taylor',
+            score: 12
+          },
+          {
+            name: 'Adrienne Raphael',
+            score: 12
+          }
+        ],
+        mmen: [
+          {
+            name: 'Joseph',
+            score: 60
+          },
+        ],
+        gmen: [
+          {
+            name: 'Joseph',
+            score: 80
+          }
+        ]
+        
+      }
